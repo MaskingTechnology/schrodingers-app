b7df004de185181eb964c0d3bd90b1ed75afb9a8
Peter van Vliet <peter@masking.tech>
2026-06-24 10:59:24 +0200

    #1: improvements

--- src/bar/app/components/main.tsx
+++ src/bar/app/components/main.tsx
@@ -1,5 +1,4 @@
 
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { DesignSystem } from '@schrodinger/common/designsystem';
@@ -7,9 +6,7 @@ import { DesignSystem } from '@schrodinger/common/designsystem';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <DesignSystem>
-        <App></App>
-      </DesignSystem>
-  </React.StrictMode>
+  <DesignSystem>
+    <App></App>
+  </DesignSystem>
 );
--- src/terrace/app/components/main.tsx
+++ src/terrace/app/components/main.tsx
@@ -1,15 +1,12 @@
 
 import { DesignSystem } from '@schrodinger/common/designsystem';
 
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <DesignSystem>
-      <App tableNumber='42'></App>
-    </DesignSystem>
-  </React.StrictMode>
+  <DesignSystem>
+    <App tableNumber='42'></App>
+  </DesignSystem>
 );
